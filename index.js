c5820bdd9298311487f1adb52f351a9001955c25
John McCambridge <johnmccambridge@Johns-MacBook-Pro.local>
2017-09-08 11:20:59 -0500

    submit

--- index.js
+++ index.js
@@ -1,124 +1,171 @@
-(function() {
-  'use strict';
+window.onload = () => {
 
-  let brushClass = 'drab-black';
 
-  const paintPixel = function(pixel) {
-    pixel.classList.remove(pixel.classList.item(1));
-    pixel.classList.add(brushClass);
+  let finalBoss = {
+    //the grid and other element stuff
+
+    board: {
+      margin: 'auto',
+      marginTop: '15px',
+      padding: '20px',
+      width: '800px',
+      height: '600px',
+      // borderRadius: '3px',
+      backgroundColor: 'transparent',
+    },
+    toolBar: {
+      margin: 'auto',
+      marginTop: '25px',
+      padding: '20px',
+      width: 'auto',
+      height: '55px',
+      borderRadius: '3px',
+      backgroundColor: 'grey',
+      boxShadow: '3px 3px 3px grey',
+      // background-image: url('https://1.bp.blogspot.com/-hMLICEzhVH0/WXZSMnCkDJI/AAAAAAAACG4/d5wfzfVE8rkNq6re8x1BNV5b04IOp-8bACLcBGAs/s1600/22222_1.gif');
+
+
+    },
+    thing: {
+      width: '8px',
+      height: '8px',
+      border: '1px solid lightgrey',
+      backgroundColor: 'white',
+      float: 'left',
+    },
+    palette: {
+      margin: 'auto',
+      display: 'flex',
+      borderRadius: '3px',
+      justifyContent: 'center',
+    },
+    button: {
+      borderRadius: '100%',
+      height: '30px',
+      width: '30px',
+    },
+    colorShit: {
+      height: 'auto',
+      width: '300px',
+      margin: 'auto',
+      marginTop: '10px',
+      textAlign: 'center'
+    },
+    colors: ['white', 'blue', 'black'],
+    currentPaintColor: null,
+    toggleMouse: false,
   }
 
-  const drawCanvas = function() {
-    const canvas = document.querySelector('#canvas');
+  //Make each pixel
+  function makeTheThingz(index) {
+    //Make the canvas
+    let thing = document.createElement('div');
+    //Add an id
+    thing.setAttribute('id', index)
 
-    for (let i = 0; i < 2013; i++) {
-      const pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      canvas.appendChild(pixel);
-    }
-
-    canvas.addEventListener('click', (event) => {
-      if (event.target === canvas) {
-        return;
+    thing.addEventListener('mouseenter', function() {
+      if (finalBoss.toggleMouse) {
+        thing.style.backgroundColor = finalBoss.currentPaintColor;
       }
-
-      paintPixel(event.target);
-    });
-
-    // Bonus 1 - paintbrush
-
-    let isPainting = false;
-
-    canvas.addEventListener('mousedown', () => {
-      isPainting = true;
-
-      if (event.target === canvas) {
-        return;
-      }
-
-      paintPixel(event.target);
-    });
-
-    document.addEventListener('mouseup', () => {
-      isPainting = false;
-    });
-
-    const pixels = document.querySelectorAll('.pixel');
-
-    for (const pixel of pixels) {
-      pixel.addEventListener('mouseenter', () => {
-        if (isPainting) {
-          paintPixel(event.target);
-        }
-      });
-    }
+    })
+    //Style the thing
+    addShit(finalBoss, thing, "thing")
+    //Add to the board
+    board.append(thing);
   }
 
-  const drawPalette = function() {
-    const palette = document.querySelector('#palette');
-
-    const colorClasses = [
-      'carmine-red',
-      'coral-red',
-      'pastel-red',
-      'tigers-eye-orange',
-      'pastel-orange',
-      'topaz-orange',
-      'sandstorm-yellow',
-      'lemon-yellow',
-      'pastel-yellow',
-      'lime-green',
-      'ufo-green',
-      'mint-green',
-      'cerulean-blue',
-      'ultramarine-blue',
-      'carolina-blue',
-      'purple-heart',
-      'lavender-indigo',
-      'bright-lavender',
-      'bruise-purple',
-      'heliotrope-purple',
-      'lavender-magenta',
-      'black-black',
-      'drab-black',
-      'dim-gray',
-      'manatee-gray',
-      'pastel-gray',
-      'white-white',
-      'bistre-brown',
-      'noir-brown',
-      'bole-brown',
-      'chestnut-brown',
-      'grullo-brown'
-    ];
-
-    for (const colorClass of colorClasses) {
-      const color = document.createElement('div');
-      color.classList.add('color', colorClass);
-      palette.appendChild(color);
+  //Create toolbar
+  function builToolbarShit() {
+    let palette = document.createElement('div');
+    addShit(finalBoss, palette, 'palette')
+    toolBar.append(palette);
+    //iterate through the colors
+    for (color of finalBoss.colors) {
+      let button = document.createElement('button');
+      button.setAttribute('id', color);
+      button.onfocus = button.style.outline = 'none';
+      /////////////////////////////////////////////////////
+      button.addEventListener('click', function() {
+        finalBoss.currentPaintColor = this.id;
+      })
+      addShit(finalBoss, button, "button", color)
+      palette.append(button)
     }
 
-    const heading = document.createElement('h2');
-    heading.textContent = 'BRUSH COLOR >';
 
-    const brushIndicator = document.createElement('div');
-    brushIndicator.classList.add('brushIndicator', brushClass);
 
-    palette.appendChild(heading);
-    palette.appendChild(brushIndicator);
 
-    palette.addEventListener('click', (event) => {
-      if (event.target === palette) {
-        return;
-      }
+    let colorShit = document.createElement('form');
 
-      brushClass = event.target.classList.item(1);
 
-      brushIndicator.classList.remove(brushIndicator.classList.item(1));
-      brushIndicator.classList.add(brushClass);
-    });
+
+    let colorInput = document.createElement('input');
+    colorInput.setAttribute('type', 'color');
+    addShit(finalBoss, colorShit, "colorShit");
+    colorInput.onfocus = colorInput.style.outline = 'none';
+    colorInput.addEventListener('change', function() {
+      finalBoss.currentPaintColor = colorInput.value;
+    })
+    toolBar.append(colorShit);
+    colorShit.append(colorInput);
   }
 
-  drawCanvas();
-  drawPalette();
-})();
+
+
+
+  let body = document.getElementsByTagName('body')[0]
+
+  let board = document.createElement('div')
+
+  let toolBar = document.createElement('div')
+
+  //Style the shit
+  addShit(finalBoss, board, "board");
+  //add style to the toolbar
+  addShit(finalBoss, toolBar, "toolBar");
+
+  // add the toolbar and board itself to the body
+  body.append(board)
+  body.append(toolBar)
+
+  //Create toolbar components
+  builToolbarShit();
+
+  //Make all the things
+  for (var i = 0; i < 1360; i++) {
+    makeTheThingz(i);
+  }
+
+  window.addEventListener('mousedown', function() {
+    finalBoss.toggleMouse = true;
+  })
+  window.addEventListener('mouseup', function() {
+    finalBoss.toggleMouse = false;
+  })
+}
+
+function addShit(options, element, objname, color) {
+  let styles = options[objname];
+  let style = element.style;
+  Object.assign(style, styles);
+  if (color) {
+    element.style.backgroundColor = color
+  }
+}
+
+// create button for html2canvas
+var button = document.createElement("button");
+button.innerHTML = "Create image file";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener("click", function() {
+  html2canvas(document.body, {
+    onrendered: function(canvas) {
+      document.body.appendChild(canvas);
+    },
+    width: 300,
+    height: 300
+  });
+});
